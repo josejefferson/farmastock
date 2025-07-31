a758092213d73d7d211db6c4d9d2953e5a49ac76
Jefferson Dantas <jeffersonn.jjdaj@gmail.com>
2025-07-31 09:59:33 -0300

    feat: Adicionado tipos das boxes

--- lib/data/boxes.dart
+++ lib/data/boxes.dart
@@ -1,7 +1,12 @@
+import 'package:farmastock/modelo/dados_farmacia.dart';
+import 'package:farmastock/modelo/entrada_estoque_modelo.dart';
+import 'package:farmastock/modelo/produto_modelo.dart';
+import 'package:farmastock/modelo/saidas_estoque_modelo.dart';
+import 'package:farmastock/modelo/usuario_modelo.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 
-late Box usuariosBox;
-late Box dadosFarmaciaBox;
-late Box entradaEstoqueBox;
-late Box produtoBox;
-late Box saidasEstoqueBox;
+late Box<UsuarioModelo> usuariosBox;
+late Box<DadosFarmacia> dadosFarmaciaBox;
+late Box<EntradaEstoque> entradaEstoqueBox;
+late Box<Produto> produtoBox;
+late Box<SaidaEstoque> saidasEstoqueBox;
