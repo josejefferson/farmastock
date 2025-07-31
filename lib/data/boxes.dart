8c3d11c1c12dc8d99d4d7717cabdb3658bb5d9ab
Jefferson Dantas <jeffersonn.jjdaj@gmail.com>
2025-07-31 09:49:42 -0300

    feat: Adicionado boxes

---
+++ lib/data/boxes.dart
@@ -0,0 +1,7 @@
+import 'package:hive_flutter/hive_flutter.dart';
+
+late Box usuariosBox;
+late Box dadosFarmaciaBox;
+late Box entradaEstoqueBox;
+late Box produtoBox;
+late Box saidasEstoqueBox;
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,28 @@
+import 'package:farmastock/data/boxes.dart';
+import 'package:farmastock/modelo/dados_farmacia.dart';
+import 'package:farmastock/modelo/entrada_estoque_modelo.dart';
+import 'package:farmastock/modelo/produto_modelo.dart';
+import 'package:farmastock/modelo/saidas_estoque_modelo.dart';
+import 'package:farmastock/modelo/usuario_modelo.dart';
 import 'package:farmastock/pages/dashboard_page.dart';
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 
 void main() async {
-  // WidgetsFlutterBinding.ensureInitialized();
+  WidgetsFlutterBinding.ensureInitialized();
+  await Hive.initFlutter();
+
+  Hive.registerAdapter(UsuarioModeloAdapter());
+  Hive.registerAdapter(DadosFarmaciaAdapter());
+  Hive.registerAdapter(EntradaEstoqueAdapter());
+  Hive.registerAdapter(ProdutoAdapter());
+  Hive.registerAdapter(SaidaEstoqueAdapter());
+
+  usuariosBox = await Hive.openBox<UsuarioModelo>('usuariosBox');
+  dadosFarmaciaBox = await Hive.openBox<DadosFarmacia>('dadosFarmaciaBox');
+  entradaEstoqueBox = await Hive.openBox<EntradaEstoque>('entradaEstoqueBox');
+  produtoBox = await Hive.openBox<Produto>('produtoBox');
+  saidasEstoqueBox = await Hive.openBox<SaidaEstoque>('saidasEstoqueBox');
 
   // final directory = await getApplicationDocumentsDirectory();
   // Hive.defaultDirectory = directory.path;
