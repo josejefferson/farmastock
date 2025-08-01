c5d094be8ec206a62ce5a9e0ff868ff4457dab41
kayo.ronald.oliveira <kayo.ronald.oliveira@gmail.com>
2025-08-01 00:32:47 +0000

    feat: Adicionar tela de entrada de estoque com Hive

--- lib/pages/entrada_estoque_page.dart
+++ lib/pages/entrada_estoque_page.dart
@@ -1,24 +1,68 @@
 import 'package:farmastock/constants/exemplo_produtos.dart';
+import 'package:farmastock/data/boxes.dart';
 import 'package:farmastock/pages/entrada_medicamento_page.dart';
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:collection/collection.dart';
 
-class EntradaEstoquePage extends StatelessWidget {
+class EntradaEstoquePage extends StatefulWidget {
   const EntradaEstoquePage({super.key});
 
   @override
+  State<EntradaEstoquePage> createState() => _EntradaEstoquePageState();
+}
+
+class _EntradaEstoquePageState extends State<EntradaEstoquePage> {
+  @override
+  void initState() {
+    entradaEstoqueBox.listenable().addListener(_onBoxChanged);
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    entradaEstoqueBox.listenable().removeListener(_onBoxChanged);
+    super.dispose();
+  }
+
+  void _onBoxChanged() {
+    setState(() {});
+  }
+
+  @override
   Widget build(BuildContext context) {
+    final entradaFiltrado =
+        entradaEstoqueBox.values
+            .where(
+              (entrada) => produtoBox.values.any(
+                (produto) => produto.id == entrada.produtoId,
+              ),
+            )
+            .toList();
+
     return Scaffold(
       appBar: AppBar(title: const Text('Entradas de Estoque')),
       body: SafeArea(
         child: ListView.separated(
-          itemCount: produtosExemplo.length,
+          itemCount: entradaFiltrado.length,
           separatorBuilder: (context, index) => const Divider(height: 1),
           itemBuilder: (context, index) {
-            final produto = produtosExemplo[index];
+            final entrada = entradaFiltrado.elementAt(index);
+
+            final produto = produtoBox.values.firstWhereOrNull(
+              (p) => p.id == entrada.produtoId,
+            );
+
+            if (produto == null) {
+              return const Text("Não há produtos cadastrados");
+            }
+
             return ListTile(
-              title: Text(produto['nome']! as String),
-              subtitle: Text(produto['descricao']! as String),
-              trailing: Text(produto['dataEntrada']! as String),
+              title: Text(produto.nome),
+              subtitle: Text(
+                "${entrada.quantidade} - ${produto.unidadeMedida.name}",
+              ),
+              trailing: Text(entrada.dataEntrada),
             );
           },
         ),
